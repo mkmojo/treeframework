af614625168470e33a3761ab8415dbc15012a294
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-01-09 22:18:36 -0500

    can compile but all code in treedef.hpp

--- lib/treedef.cpp
+++ lib/treedef.cpp
@@ -1,200 +1,125 @@
-/** @file treedef.cpp
- *
- *  @brief This file contains implementations of base classes and default templates of the tree framework
- *
- *  @author Shule Li <shuleli@berkeley.edu> Qiyuan Qiu <qiuqiyuan@gmail.com> Saminda Wijeratne <>
- *
- *  @date 12-20-14
- *  
- *  @bug No known bugs.
- *
- *  @copyright Rochester Software License
- */
-
-#include "./treedef.hpp"
-using namespace treedef;
-
-/** @brief This function contructs a tree node. U: input data type, V: combined type
- *  @param pVar_in pointer to the input data structure
- */
-template <typename U, typename V> 
-TreeNode<U, V>::TreeNode(U* pVar_in):dGen(0),dOwn(0) {
-    //pVar = pVar_in; //< copy to shared pointer
-    //pGen = new NodeSet<U, V>; //< initialize generate set
-    //pOwn = new NodeSet<U, V>; //< initialize owner set
-}
-
-
-/** @brief This function destroys a tree node. U: input data type, V: combined type
-*/
-template<typename U, typename V> 
-TreeNode<U,V>::~TreeNode(){
-    //pGen->clear(); //< clear the generate and owner sets
-    //pOwn->clear();
-}
-
-
-/** @brief This function contructs a generate set from a user defined generate list. U: input data type, V: combined type
- *  @param list_in pointer to the input list
- *  @return void
- */
-template<typename U, typename V> 
-void TreeNode<U,V>::getGenSet(NodeList<U, V>* list_in){
-    if(list_in->empty()) return;
-    for(auto it = list_in.begin(); it != list_in.end(); it++){
-        this->pGen->insert(*it); //< insert list nodes into the generate set
-        (*it)->pOwn->insert(this); //< insert this into the list nodes' own set
-        (*it)->dOwn++; //< increment the owner's counter
-    }
-    this->dGen = list_in.size(); //< set generate counter to be the length of the list
-
-    list_in->clear(); //< clear the list
-    delete(list_in);
-}
-
-
-/** @brief This function contructs a generate list for one node. It needs to be specialized by the user. GenID: type of generate, T: input data type, R: combined type
- *  @param u pointer to the node being worked on
- *  @return NodeList
- */
-template <const std::string& GenID, typename T, typename R> NodeList<T,R>* Generate(NodePtr<T, R> u) {
-    //> default generate function for tree:
-    auto pChild = u->left;
-    auto tmplist = new NodeList<T, R>;
-
-    //> push all children into the list
-    do{
-        tmplist->push_back(pChild);
-    } while(pChild.sibling != nullptr);
-    return tmplist;
-}
-
-
-/** @brief This function combines input data type stored in v into u. It needs to be specialized by the user. ComID: type of combine, T: input data type, R: combined type
- *  @param u pointer to the data structure storing the combined value
- *  @param v pointer to the data structure storing the input value
- *  @return void 
- */
-template <const std::string& ComID, typename T, typename R> void Combine(R* u, T* v) {
-    //>default: add all child node's input
-    *u += *v;
-}
-
-
-/** @brief This function evolves the input data type stored in v based on combined value stored in u. It needs to be specialized by the user. EvoID: type of evolve, T: input data type, R: combined type
- *  @param u pointer to the data structure storing the combined value
- *  @param v pointer to the data structure storing the input value
- *  @return void 
- */
-template<const std::string& EvoID, typename T, typename R> void Evolve(R* u, T* v) {
-    //>default: set the new input to be the old combine
-    v = u;
-}
-
-
-/** @brief This function adapts user defined combine into tree combine. ComID: type of combine, T: input data type, R: combined type
- *  @param u pointer to the data structure storing the combined value
- *  @param v pointer to the data structure storing the input value
- *  @return void 
- */
-template<const std::string& ComID, typename T, typename R> void TreeCombine(NodePtr<T,R> u, NodePtr<T,R> v){
-    Combine<ComID, T, R>(u->pCom, v->pVar);
-    u->dGen--; //< decrease the gen counter and the own counter
-    v->dOwn--; //< a tree node with dOwn = 0 is considered to be removed from the residual tree
-}
-
-
-/** @brief This function constructs a base type tree from an input data filestream. T: input data type, R: combined type
- *  @param file_in filestream pointing to input data
- */
-template<typename T, typename R> 
-BaseTree<T,R>::BaseTree(std::ifstream& file_in){
-    auto tmp = std::getline(file_in);
-    Root = new TreeNode<T,R>(tmp);
-    while(tmp != nullptr){
-        tmp = std::getline(file_in);
-        this->Insert(tmp); //< add to tree
-        TreeNodeList.pushback(tmp); //< add to nodelist
-    }
-}
-
-
-/** @brief This function destroys a base type tree. T: input data type, R: combined type
- *  @param file_in filestream pointing to input data
- */
-template<typename T, typename R> BaseTree<>::~BaseTree(){
-    TreeNodeList.clear(); //< delete all nodes associated with this tree
-}
-
-
-/** @brief This function finds the common ancestor of nodes u and v. T: input data type, R: combined type
- *  @param u pointer to the first node
- *  @param v pointer to the second node
- *  @return NodePtr
- */
-template<typename T, typename R> NodePtr<T,R> BaseTree::getCommonAncestor(NodePtr<T,R> u, NodePtr<T,R> v){
-    //...
-}
-
-
-/** @brief This function creates and inserts a tree node storing input data value of data_in. T: input data type, R: combined type
- *  @param data_in pointer to the first node
- *  @return void
- */
-template<typename T, typename R> void BaseTree::Insert(T* data_in){
-    //...
-}
-
-
-/** @brief This function collapses the basetree from node u up. T: input data type, R: combined type
- *  @param u node considered
- *  @return void
- */
-template<typename T, typename R> void Collapse(NodePtr<T,R> u){
-    if(u->dGen != 0) return;
-    auto it = u;
-    while(it->parent != nullptr && it->parent->dGen <= 1){
-        *(it->parent->pCom) = *(it->pCom); //< store the combined value into its parent
-        Remove(it);
-        it = it->parent;
-    }
-}
-
-
-/** @brief This function do one pass of tree computation involving three steps: generate, combine and evolve. T: input data type, R: combined type
- *  @param strict if the computation has dependency on the newly combined values
- *  @param ComID type of combine
- *  @param EvoID type of evolve
- *  @param GenID type of generate: default to null
- *  @return void
- */
-template<typename T, typename R> void BaseTree::tree_compute(bool strict, const std::string& ComID, const std::string& EvoID, const std::string& GenID = "null"){
-    //> note: needs to figure out the direction of tree compute based on generate
-    if(GenID == "null"){
-        if(std::string dummy_str= "some node has node.genset -> nullptr") 
-            throw UnknownGenerateSetException;
-    }else{
-        for(auto it = TreeNodeList.begin(); it != TreeNodeList.end(); it++){
-            (*it)->pGen->clear(); //< clear the generate and owner sets for all nodes before inserting
-            (*it)->pOwn->clear();
+namespace treedef{
+    /** @brief This function contructs a tree node. U: input data type, V: combined type
+     *  @param pVar_in pointer to the input data structure
+     */
+    template <typename U, typename V> 
+        TreeNode<U, V>::TreeNode(U* pVar_in):dGen(0),dOwn(0) {
+            pVar = pVar_in; //< copy to shared pointer
+            pGen = new NodeSet<U, V>; //< initialize generate set
+            pOwn = new NodeSet<U, V>; //< initialize owner set
         }
-        for(auto it = TreeNodeList.begin(); it != TreeNodeList.end(); it++)
-            (*it)->getGenSet(Generate<GenID, T, R>(*it)); //have all gensets of nodes figured out
-        if(strict) TreeNodeList.sort(); //< sort needs to be overloaded: this sorting needs to reflect how the tree nodes are removed from the residual tree
-        for(auto it = TreeNodeList.begin(); it != TreeNodeList.end(); it++){
-            for(auto itt = (*it)->pGen.begin(); itt != (*it)->pGen.end(); itt++){
-                TreeCombine<ComID, T, R>(*it, *itt); //< call tree combine with dependency
-                //< note: necessary markings need to be done during combine
-                if(!(*it->dGen)){ //< all generates are considered for it
-                    if(strict) Evolve<EvoID, T, R>(*it); //< update pVar with the combined value for positive dependency case
-                    Collapse(*it); //< collapse from *it till a node with dGen > 1
-                }
+
+
+    /** @brief This function destroys a tree node. U: input data type, V: combined type
+    */
+    template<typename U, typename V> 
+        TreeNode<U,V>::~TreeNode(){
+            pGen->clear(); //< clear the generate and owner sets
+            pOwn->clear();
+        }
+
+
+    /** @brief This function contructs a generate set from a user defined generate list. U: input data type, V: combined type
+     *  @param list_in pointer to the input list
+     *  @return void
+     */
+    template<typename U, typename V> 
+        void TreeNode<U,V>::getGenSet(NodeList<U, V>* list_in){
+            if(list_in->empty()) return;
+            for(auto it = list_in.begin(); it != list_in.end(); it++){
+                this->pGen->insert(*it); //< insert list nodes into the generate set
+                (*it)->pOwn->insert(this); //< insert this into the list nodes' own set
+                (*it)->dOwn++; //< increment the owner's counter
             }
+            this->dGen = list_in.size(); //< set generate counter to be the length of the list
+
+            list_in->clear(); //< clear the list
+            delete(list_in);
         }
-        if(!strict){
-            for(auto it = TreeNodeList.begin(); it != TreeNodeList.end(); it++)
-                Evolve<EvoID, T, R>(*it); //< update pVar with the combined value all at once for reversed or non dependency cases
+    /** @brief This function contructs a generate list for one node. It needs to be specialized by the user. GenID: type of generate, T: input data type, R: combined type
+     *  @param u pointer to the node being worked on
+     *  @return NodeList
+     */
+    template <const std::string& GenID, typename T, typename R> NodeList<T,R>* Generate(NodePtr<T, R> u) {
+        //> default generate function for tree:
+        auto pChild = u->left;
+        auto tmplist = new NodeList<T, R>;
+
+        //> push all children into the list
+        do{
+            tmplist->push_back(pChild);
+        } while(pChild.sibling != nullptr);
+        return tmplist;
+    }
+
+
+    /** @brief This function combines input data type stored in v into u. It needs to be specialized by the user. ComID: type of combine, T: input data type, R: combined type
+     *  @param u pointer to the data structure storing the combined value
+     *  @param v pointer to the data structure storing the input value
+     *  @return void 
+     */
+    template <const std::string& ComID, typename T, typename R> void Combine(R* u, T* v) {
+        //>default: add all child node's input
+        *u += *v;
+    }
+
+
+    /** @brief This function evolves the input data type stored in v based on combined value stored in u. It needs to be specialized by the user. EvoID: type of evolve, T: input data type, R: combined type
+     *  @param u pointer to the data structure storing the combined value
+     *  @param v pointer to the data structure storing the input value
+     *  @return void 
+     */
+    template<const std::string& EvoID, typename T, typename R> void Evolve(R* u, T* v) {
+        //>default: set the new input to be the old combine
+        v = u;
+    }
+
+
+    /** @brief This function adapts user defined combine into tree combine. ComID: type of combine, T: input data type, R: combined type
+     *  @param u pointer to the data structure storing the combined value
+     *  @param v pointer to the data structure storing the input value
+     *  @return void 
+     */
+    template<const std::string& ComID, typename T, typename R> void TreeCombine(NodePtr<T,R> u, NodePtr<T,R> v){
+        Combine<ComID, T, R>(u->pCom, v->pVar);
+        u->dGen--; //< decrease the gen counter and the own counter
+        v->dOwn--; //< a tree node with dOwn = 0 is considered to be removed from the residual tree
+    }
+
+
+
+
+
+    /** @brief This function finds the common ancestor of nodes u and v. T: input data type, R: combined type
+     *  @param u pointer to the first node
+     *  @param v pointer to the second node
+     *  @return NodePtr
+     */
+    template<typename T, typename R> NodePtr<T,R> BaseTree<T, R>::getCommonAncestor(NodePtr<T,R> u, NodePtr<T,R> v){
+        //...
+    }
+
+
+    /** @brief This function creates and inserts a tree node storing input data value of data_in. T: input data type, R: combined type
+     *  @param data_in pointer to the first node
+     *  @return void
+     */
+    template<typename T, typename R> void BaseTree<T, R>::Insert(T* data_in){
+        //...
+    }
+
+
+    /** @brief This function collapses the basetree from node u up. T: input data type, R: combined type
+     *  @param u node considered
+     *  @return void
+     */
+    template<typename T, typename R> void Collapse(NodePtr<T,R> u){
+        if(u->dGen != 0) return;
+        auto it = u;
+        while(it->parent != nullptr && it->parent->dGen <= 1){
+            *(it->parent->pCom) = *(it->pCom); //< store the combined value into its parent
+            Remove(it);
+            it = it->parent;
         }
     }
 }
-
--- lib/treedef.hpp
+++ lib/treedef.hpp
@@ -17,7 +17,9 @@
 #include <memory>
 #include <exception>
 #include <vector>
-#include "MessageBuffer.h"
+#include <fstream>
+#include <iostream>
+//#include "MessageBuffer.h"
 
 namespace treedef {
     //> type declarations and short hands
@@ -25,13 +27,13 @@ namespace treedef {
     template <typename U, typename V> using NodePtr = std::shared_ptr<TreeNode<U, V> >; //< this typedef only works for 0x
     template <typename U, typename V> using NodeList = std::vector<std::shared_ptr<TreeNode<U, V> > >; //< this typedef only works for 0x
     template <typename U, typename V> using NodeSet = std::set<std::shared_ptr<TreeNode<U, V> > >; //< this typedef only works for 0x
-    template <const std::string& GenID, typename T, typename R> NodeList* Generate(NodePtr<T, R>);
+    template <const std::string& GenID, typename T, typename R> NodeList<T, R>* Generate(NodePtr<T, R>);
     template <const std::string& ComID, typename T, typename R> void Combine(T*, R*);
     template <const std::string& EvoID, typename T, typename R> void Evolve(T*, R*);
     template <const std::string& ComID, typename T, typename R> void TreeCombine(NodePtr<T,R>, NodePtr<T,R>);
     template <typename T, typename R> class BaseTree;
-    MessageBuffer m_comm;
-    
+    //MessageBuffer m_comm;
+
 
     /** @brief This class stores customized error message
      *  can be derived further to include more detailed error
@@ -47,21 +49,20 @@ namespace treedef {
      *  @param V combined data type
      */
     template <typename U, typename V> class TreeNode {
-        typedef std::shared_ptr<typename U> VarPtr;
-        typedef std::shared_ptr<typename V> ComPtr;
+        typedef std::shared_ptr<U> VarPtr;
+        typedef std::shared_ptr<V> ComPtr;
 
         public:
         VarPtr pVar; //< pointer to the input field of a tree node
         ComPtr pCom; //< pointer to the combined field of a tree node
-        NodeSet *pGen, *pOwn; //< pointer to the generate set and owner set
+        NodeSet<U, V> *pGen, *pOwn; //< pointer to the generate set and owner set
         int dGen, dOwn;  //< generate and owner counter for the residual tree 
-        NodePtr<T, R> left, parent, sibling; //< pointer to the neighbors
+        NodePtr<U, V> left, parent, sibling; //< pointer to the neighbors
 
         TreeNode(U*); //< constructor: a pointer to the initial input field value is passed in
         ~TreeNode(); //< destructor
         void getGenSet(NodeList<U, V>*); //< converts a list of node pointers into generate set
     };
-
 
     /** @brief This class stores a base type tree. Different types of trees can be derived from this.
      *  @param T input data type
@@ -77,10 +78,47 @@ namespace treedef {
             NodeList<T,R> TreeNodeList;  //< stores the entire list of the tree nodes. in case of array implementation, this should be a vector instead of list
 
         public: 
-            BaseTree(std::ifstream); //< constructs a base tree from an input filestream 
-            virtual ~BaseTree(); //< destructor. needs overriding
-            void tree_compute(bool, std::string, std::string, std::string); //< calls generate or combine templates. specialized tree_compute can be implemented for different types of trees by overriding
+            BaseTree(){}; //< constructs a base tree from an input filestream 
+            ~BaseTree(){}; //< destructor. needs overriding
+            void tree_compute(bool, const std::string&, const std::string&, const std::string&); //< calls generate or combine templates. specialized tree_compute can be implemented for different types of trees by overriding
     };
 
+    /** @brief This function do one pass of tree computation involving three steps: generate, combine and evolve. T: input data type, R: combined type
+     *  @param strict if the computation has dependency on the newly combined values
+     *  @param ComID type of combine
+     *  @param EvoID type of evolve
+     *  @param GenID type of generate: default to null
+     *  @return void
+     */
+    template<typename T, typename R> void BaseTree<T, R>::tree_compute(bool strict, const std::string& ComID, const std::string& EvoID, const std::string& GenID = "null"){
+        //> note: needs to figure out the direction of tree compute based on generate
+        if(GenID == "null"){
+            if(std::string dummy_str = "some node has node.genset -> nullptr") {
+                //throw UnknownGenerateSetException;
+            }
+        }else{
+            for(auto it = TreeNodeList.begin(); it != TreeNodeList.end(); it++){
+                (*it)->pGen->clear(); //< clear the generate and owner sets for all nodes before inserting
+                (*it)->pOwn->clear();
+            }
+            for(auto it = TreeNodeList.begin(); it != TreeNodeList.end(); it++)
+                (*it)->getGenSet(Generate<GenID, T, R>(*it)); //have all gensets of nodes figured out
+            if(strict) TreeNodeList.sort(); //< sort needs to be overloaded: this sorting needs to reflect how the tree nodes are removed from the residual tree
+            for(auto it = TreeNodeList.begin(); it != TreeNodeList.end(); it++){
+                for(auto itt = (*it)->pGen.begin(); itt != (*it)->pGen.end(); itt++){
+                    TreeCombine<ComID, T, R>(*it, *itt); //< call tree combine with dependency
+                    //< note: necessary markings need to be done during combine
+                    if(!(*it->dGen)){ //< all generates are considered for it
+                        if(strict) Evolve<EvoID, T, R>(*it); //< update pVar with the combined value for positive dependency case
+                        Collapse(*it); //< collapse from *it till a node with dGen > 1
+                    }
+                }
+            }
+            if(!strict){
+                for(auto it = TreeNodeList.begin(); it != TreeNodeList.end(); it++)
+                    Evolve<EvoID, T, R>(*it); //< update pVar with the combined value all at once for reversed or non dependency cases
+            }
+        }
+    }
 }
-
+#include "treedef.cpp"
