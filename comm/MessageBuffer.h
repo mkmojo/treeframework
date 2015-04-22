d511625769809dea1f19448c44a059583cf3b5a5
Shule Li <shull200@gmail.com>
2015-04-21 20:59:37 -0400

    added message layer

---
+++ comm/MessageBuffer.h
@@ -0,0 +1,65 @@
+#ifndef MESSAGE_BUFFER_H
+#define MESSAGE_BUFFER_H 1
+class MessageBuffer;
+
+#include "CommLayer.h"
+#include "Messages.h"
+#include <vector>
+#include <assert.h>
+
+typedef std::vector<Message*> MsgBuffer;
+typedef std::vector<MsgBuffer> MessageQueues;
+
+enum SendMode
+{
+    SM_BUFFERED,
+    SM_IMMEDIATE
+};
+
+class MessageBuffer : public CommLayer
+{
+    public:
+        MessageBuffer();
+
+        void sendCheckPointMessage(int argument = 0)
+        {
+            assert(transmitBufferEmpty());
+            CommLayer::sendCheckPointMessage(argument);
+        }
+
+        void sendControlMessage(APControl command, int argument = 0)
+        {
+            assert(transmitBufferEmpty());
+            CommLayer::sendControlMessage(command, argument);
+        }
+
+        void sendControlMessageToNode(int dest,
+                APControl command, int argument = 0)
+        {
+            assert(transmitBufferEmpty());
+            CommLayer::sendControlMessageToNode
+                (dest, command, argument);
+        }
+
+        void sendSeqAddMessage(int procID, 
+                const Point& p);
+
+        void sendSeqSortMessage(int procID,
+                const Point& P);
+
+        bool transmitBufferEmpty() const;
+
+        void flush();
+        void clearQueue(int procID);
+        void queueMessage
+            (int nodeID, Message* message, SendMode mode);
+
+        //APMessage checkMessage(int senderID);
+
+        void checkQueueForSend(int procID, SendMode mode);
+    private:
+        static const size_t MAX_MESSAGES = 100;
+        MessageQueues m_msgQueues;
+};
+
+#endif
