d41a17abf06bab19515e13aeb791f46955726331
samindaw <samindaw@gmail.com>
2015-07-03 17:38:39 -0400

    adding utils

---
+++ lib/DataUtils.hpp
@@ -0,0 +1,23 @@
+/*
+ * DataUtils.hpp
+ *
+ *  Created on: Jul 3, 2015
+ *      Author: samindaw
+ */
+
+#ifndef LIB_DATAUTILS_HPP_
+#define LIB_DATAUTILS_HPP_
+
+#include <sstream>
+
+
+namespace data_utils{
+
+	size_t copyData(void* mem, const void* p, size_t size){
+		std::memcpy(mem, p, size);
+		return size;
+	}
+
+}
+
+#endif /* LIB_DATAUTILS_HPP_ */
