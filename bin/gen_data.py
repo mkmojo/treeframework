6ab619164d11a7971c03c79337da6966dac28026
Chirag Jain <cjain7@gatech.edu>
2015-07-09 11:53:14 -0500

    adding input data gen and script gen

---
+++ bin/gen_data.py
@@ -0,0 +1,24 @@
+import random
+import sys
+
+def generate_input(filename, num, levels):
+    with open(filename, 'w') as f:
+        f.write("NDIM 3"+'\n')
+        f.write("MAX_LEVEL "+levels+'\n'+'\n')
+        f.write("DATA_TABLE"+'\n')
+        for num_line in range(int(num)):
+            x = str(random.uniform(1, 10))
+            y = str(random.uniform(1, 10))
+            z = str(random.uniform(1, 10))
+            mass = str(random.uniform(1, 10))
+            f.write(" ".join( [x, y, z, mass]) + '\n')
+
+def main():
+    if len(sys.argv) != 4:
+        print "error, must have 3 arguments"
+        print "filename, n_points, m_levels"
+        exit(1)
+    generate_input(sys.argv[1],sys.argv[2], sys.argv[3])
+
+if __name__ == "__main__":
+    main()
